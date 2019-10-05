74d18ea31d500b058e5e4b5e09a6d4a99bbcdf52
Frank Théodoloz <frank@16d.ch>
2019-10-05 07:33:33 +0200

    Exercice 6 terminé

--- ex6/exercice_1.py
+++ ex6/exercice_1.py
@@ -1,14 +1,35 @@
-def power(a: int, n: int) -> int:
+def power_iterative(a: int, n: int) -> int:
     """
     Calculer a^n
     :param a: nombre entier
     :param n: exposant
     :return: résultat de a^n
     """
-    #TODO: Votre code ici
-    return -1
+    iter = 0
+    result = a
+    for i in range(1, n):
+        iter += 1
+        result = result * a
+    print("itérations =", iter)
+    return result
+
+
+def power_recursive(a: int, n: int, iteration: int = 0) -> int:
+    """
+    Calculer a^n
+    :param a: nombre entier
+    :param n: exposant
+    :param iteration: compteur
+    :return: résultat de a^n
+    """
+
+    if n == 0:
+        # print("itérations =", iteration)
+        return 1
+        # iteration += 1
+    return a * power_recursive(a, n - 1, iteration)
 
 
 if __name__ == '__main__':
-    # Vos tests ici
-    pass
+    # print(power_iterative(3, 16))
+    print(power_recursive(3, 16))
--- ex6/exercice_2.py
+++ ex6/exercice_2.py
@@ -1,3 +1,22 @@
+def power_test(a: int, n: int, iter: int) -> int:
+    """
+    Calculer a^n en ayant une performance de log(n)
+    n doit être un pouvoir de 2
+    :param a: nombre entier
+    :param n: exposant
+    :param iter: nombre entier pour compter les itérations
+    :return: résultat de a^n
+    """
+    if n == 1:
+        print("itération =", iter)
+        return a
+    elif n % 2 != 0:
+        print("l'exposant utilisé sera le pouvoir de 2 inférieur.")
+
+    iter += 1
+    return power_test(a * a, int(n / 2), iter)
+
+
 def power(a: int, n: int) -> int:
     """
     Calculer a^n en ayant une performance de log(n)
@@ -6,10 +25,11 @@ def power(a: int, n: int) -> int:
     :param n: exposant
     :return: résultat de a^n
     """
-    #TODO: Votre code ici
-    return -1
+
+    if n == 1: return a
+    return power(a * a, int(n / 2))
 
 
 if __name__ == '__main__':
-    # Vos tests ici
-    pass
+    # print(power_test(3, 16, 0))
+    print(power(3, 16))  # 16 = 4 itérations
